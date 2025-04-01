1769583be0dafd407f880426d7ffdf64f32a13b8
MacMffin <Tomvanzanten9@hotmail.com>
2025-04-01 20:25:21 +0200

    Initial commit

---
+++ src/test/java/nl/test/BuilderResourceIT.java
@@ -0,0 +1,8 @@
+package nl.test;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class BuilderResourceIT extends BuilderResourceTest {
+    // Execute the same tests but in packaged mode.
+}
